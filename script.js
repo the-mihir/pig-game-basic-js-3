d8066760bdd976f3660042a0e276da0e99f67c88
Mihir Das <mihirkantho@gmail.com>
2023-10-02 00:31:17 +0600

    updating winner score point

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ btnHold.addEventListener('click', function () {
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 200) {
             playing = false;
             // Finish the game
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
